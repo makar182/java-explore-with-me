acb55ff11ca7cb15abc53a5517b3fb59c993deea
Mark Savushkin <mark.savushkin@bimeister.com>
2023-02-21 18:22:43 +0300

    Этап №1

--- stats/stats-client/src/main/java/ru/practicum/statsclient/BaseClient.java
+++ stats/stats-client/src/main/java/ru/practicum/statsclient/BaseClient.java
@@ -24,7 +24,7 @@ public class BaseClient {
         return makeAndSendRequest(HttpMethod.POST, path, null, body);
     }
 
-    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, @Nullable Map<String, Object> parameters, @Nullable T body) {
+    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, Map<String, Object> parameters, T body) {
         HttpEntity<T> requestEntity = new HttpEntity<>(body);
 
         ResponseEntity<Object> statsServerResponse;
